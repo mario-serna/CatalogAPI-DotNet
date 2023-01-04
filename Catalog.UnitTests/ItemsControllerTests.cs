904fccd230b3af2a7bc1bd9686bfb9f5d05b98aa
Mario Serna <mario.serna@softtek.com>
2023-01-04 17:18:37 -0600

    feat: add UnitTests project

---
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -0,0 +1,14 @@
+namespace Catalog.UnitTests;
+
+public class ItemsControllerTests
+{
+    [Fact]
+    public void UnitOfWork_StateUnderTest_ExpectedBehavior()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+}
